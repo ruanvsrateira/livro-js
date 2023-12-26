fdf1b0fe181ad0c9bc9fe30921726e767044d08b
ruanvsrateira <vruan85@gmail.com>
2023-12-26 17:22:49 -0300

    feat: learning about template string

---
+++ template-string/index.js
@@ -0,0 +1,6 @@
+// A template string é utilizada para colocar os valores das váriaveis dentro de uma string
+
+const myName = "ruan";
+const myAge = 19;
+
+console.log(`Hello! My name is ${myName}, and I am ${myAge} years old.`);
