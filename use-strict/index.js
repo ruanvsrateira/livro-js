a75d945e71229dd41876eb85966b1f7bf88bf793
ruanvsrateira <vruan85@gmail.com>
2023-12-27 22:44:29 -0300

    feat: learning about use strict

---
+++ use-strict/index.js
@@ -0,0 +1,9 @@
+/* 
+  Use Strict -> String escrita use strict no ínicio do arquivo, mostra para o interpretador que o nosso código seguirá as boas práticas como: 
+                declarar se variavel é var, let ou const, não permitir que se declare variáveis com o mesmo nome de palavras reservadas 
+*/
+"use strict";
+
+// Daria erro se eu utilizasse val = 12;
+const val = 12;
+console.log({ val });
